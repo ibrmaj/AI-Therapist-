b43458f785972ec495551fa4c2f2317f15a49ab1
Ibrahim Majid <i.majidbashir@gmail.com>
2024-12-28 20:50:35 -0500

    Set up first draft of landing page

--- ai-therapist/src/App.js
+++ ai-therapist/src/App.js
@@ -1,10 +1,17 @@
+import React from 'react';
+import './index.css';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to your AI therapist!</h1>
-      <h2>We shall have a working model soon...
-    <span className="spinner"></span>
-</h2>
+    <div className="Container">
+      <div className="left-section">
+        <h1 className="main-heading">TherapyAI</h1>
+        <p className="sub-heading">Your very own AI Therapist!</p>
+        <p className="footer-text">© 2024 AI Therapist | Empowering Minds</p>
+      </div>
+      <div className="right-section">
+      <button className="cta-button">Get Started!</button>
+      </div>
     </div>
   );
 }
---
+++ ai-therapist/src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2024 AI Therapist | Empowering Minds</p>
+    </footer>
+  );
+}
+
+export default Footer;
